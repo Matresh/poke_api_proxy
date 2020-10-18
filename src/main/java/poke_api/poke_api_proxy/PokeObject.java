b64a8be77640588bf4334fd41bfbdeeaf101832d
Dima <kovalchuk.d@ajax.systems>
2020-10-18 17:06:10 +0300

    poke object class

--- src/main/java/poke_api/poke_api_proxy/PokeObject.java
+++ src/main/java/poke_api/poke_api_proxy/PokeObject.java
@@ -1,3 +1,25 @@
 package poke_api.poke_api_proxy;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PokeObject {
+
+    // TODO READ HOW CAN I MAP NAMES FROM JSON USING ANNOTATION OR SMTH LIKE THAT
+    private int id;
+    private int name;
+
+    @Override
+    public String toString() {
+        return "PokeObject{" +
+                "id=" + id +
+                ", name=" + name +
+                '}';
+    }
 }
